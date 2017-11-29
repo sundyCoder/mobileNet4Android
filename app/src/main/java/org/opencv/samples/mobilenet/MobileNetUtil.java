fe68feef6e11dc255e99e087bc7b8f62f4f002d1
sundy <sundycoder@gmail.com>
2017-11-29 14:29:55 +0800

    init.

---
+++ app/src/main/java/org/opencv/samples/mobilenet/MobileNetUtil.java
@@ -0,0 +1,11 @@
+package org.opencv.samples.mobilenet;
+
+/**
+ * Created by A1838 on 11/29/2017.
+ */
+
+public class MobileNetUtil {
+    public static final String ApplicationDir="mobileNet";
+    public static final String protoPath="MobileNet_deploy.prototxt";
+    public static final String weightsPath="MobileNet_deploy.caffemodel";
+}
